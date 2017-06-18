06e790a9104795f40ce6e0a758689d28153f8ed3
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-06-18 20:29:04 +0200

    Update formula

--- coursier.rb
+++ coursier.rb
@@ -1,9 +1,9 @@
 class Coursier < Formula
   desc "Coursier launcher."
   homepage "http://get-coursier.io"
-  url "https://repo1.maven.org/maven2/io/get-coursier/coursier-cli_2.11/1.0.0-M15-5/coursier-cli_2.11-1.0.0-M15-5-standalone.jar", :using => :nounzip
-  sha1 "bc797678fa4cadde0beada92d58d75bd3e79fc22"
-  version "1.0.0-M15-5"
+  url "https://repo1.maven.org/maven2/io/get-coursier/coursier-cli_2.11/1.0.0-RC4/coursier-cli_2.11-1.0.0-RC4-standalone.jar", :using => :nounzip
+  sha1 "332c97f6a0088d2d6ddad3087c88e103c417f9c1"
+  version "1.0.0-RC4"
 
   bottle :unneeded
 
@@ -15,7 +15,7 @@ class Coursier < Formula
 
   test do
     ENV["COURSIER_CACHE"] = "#{testpath}/cache"
-    output = shell_output("#{bin}/coursier launch io.get-coursier::echo:1.0.0-M15-5 -- foo")
+    output = shell_output("#{bin}/coursier launch io.get-coursier:echo:1.0.0-RC4 -- foo")
     assert_equal "foo", output.lines
   end
 end
