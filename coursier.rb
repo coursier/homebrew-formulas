848b7ce17f531ae34d3dba5921bfdee22d7b39d4
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-08-17 11:47:26 +0200

    Don't require updating the formula for each release
    Like paulp's original coursier formula

--- coursier.rb
+++ coursier.rb
@@ -1,21 +1,21 @@
+require 'formula'
+
+# adapted from https://github.com/paulp/homebrew-extras/blob/8184f9a962ce0758f4cf7a07b702bc1c3d16dfaa/coursier.rb
+
 class Coursier < Formula
   desc "Coursier launcher."
   homepage "http://get-coursier.io"
-  url "https://repo1.maven.org/maven2/io/get-coursier/coursier-cli_2.11/1.0.0-RC4/coursier-cli_2.11-1.0.0-RC4-standalone.jar", :using => :nounzip
-  sha256 "c2d928aea74d5d918ed333bcf0bdeeced90b5112e86d088585ff59513b926ed9"
-  version "1.0.0-RC4"
-
-  bottle :unneeded
+  head 'git://github.com/alexarchambault/coursier.git'
 
   depends_on :java => "1.8+"
 
   def install
-    bin.install Dir["*"].shift => "coursier"
+    bin.install 'coursier'
   end
 
   test do
     ENV["COURSIER_CACHE"] = "#{testpath}/cache"
-    output = shell_output("#{bin}/coursier launch io.get-coursier:echo:1.0.0-RC4 -- foo")
-    assert_equal "foo", output.lines
+    output = shell_output("#{bin}/coursier launch io.get-coursier:echo:1.0.0-RC10 -- foo")
+    assert_equal ["foo\n"], output.lines
   end
 end
