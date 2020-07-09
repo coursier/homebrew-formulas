c984749478fbf71b5b6fe83d709df59e82cb7fe5
Travis-CI <invalid@travis-ci.com>
2020-07-09 13:07:15 +0000

    Updates for 2.0.0-RC6-23

--- coursier.rb
+++ coursier.rb
@@ -5,15 +5,15 @@ require 'formula'
 class Coursier < Formula
   desc "Coursier launcher."
   homepage "https://get-coursier.io"
-  version "2.0.0-RC6-22"
-  url "https://github.com/coursier/coursier/releases/download/v2.0.0-RC6-22/cs-x86_64-apple-darwin"
-  sha256 "77b7e942796a4dc3d244769c2d03b89ecb64fbe2e45055fd057f6ee2e078b69b"
+  version "2.0.0-RC6-23"
+  url "https://github.com/coursier/coursier/releases/download/v2.0.0-RC6-23/cs-x86_64-apple-darwin"
+  sha256 "6b2627cde0001e36b8d65370fa4494c02429c8a8ae695e079759a0f8d078a3f9"
   bottle :unneeded
 
   # https://stackoverflow.com/questions/10665072/homebrew-formula-download-two-url-packages/26744954#26744954
   resource "jar-launcher" do
-    url "https://github.com/coursier/coursier/releases/download/v2.0.0-RC6-22/coursier"
-    sha256 "6d470689f62755e2ca99e09b8088fbbad4d184a250e787ad5c3e15bac2b83b98"
+    url "https://github.com/coursier/coursier/releases/download/v2.0.0-RC6-23/coursier"
+    sha256 "22ccb769a7920f98667145d4cd0526683e6979fe954042e9a6b5689b8cdcd666"
   end
 
   option "without-zsh-completions", "Disable zsh completion installation"
